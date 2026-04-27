c191a8230ff4952e5d6799f6ee71ac0572eb401d
yangk <1172977633@qq.com>
2026-04-27 09:43:55 +0800

    feat: add SUEL ERP documentation site

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "智邦国际ERP官网": "https://www.wangkangonline.com",
+      "操作手册首页": "/智邦国际ERP-操作总览",
     },
   }),
 }
